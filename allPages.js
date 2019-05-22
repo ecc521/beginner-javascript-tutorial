2c9382b1d0637f5ccfbec4e24d6b04ca4004dfd9
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-05-21 21:32:25 -0400

    Update allPages.js

--- allPages.js
+++ allPages.js
@@ -1,8 +1,6 @@
-//May not actually be on all pages, though it should be.
-
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/sw.js');
+        navigator.serviceWorker.register('sw.js');
     });
 }
 
